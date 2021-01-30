3633b9eab838fe76d4fb9f6cab19b2bde5a30dde
Jordan Ripley <jor498@usask.ca>
2021-01-29 18:48:33 -0600

    Added basic order system
    When "space" is pressed, new order appears in top left corner. Random produce and amount

---
+++ Assets/Scripts/OrderSystem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OrderSystem : MonoBehaviour
+{
+    public Text orderText;
+
+    public List<string> produce;
+
+    public int minOrderSize;
+    public int maxOrderSize;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            SetNewOrder();
+        }
+    }
+
+    private void SetNewOrder() {
+        int orderProduce = Random.Range(0, produce.Count);
+        int orderSize = Random.Range(minOrderSize, maxOrderSize);
+
+        orderText.text = produce[orderProduce] + ": " + orderSize;
+    }
+}
